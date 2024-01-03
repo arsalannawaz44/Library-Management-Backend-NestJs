66101e081dd2deebe14b35fc98c762ef70499c09
arsalannawaz44 <arsalannawaz44@gmail.com>
2024-01-03 18:08:11 +0500

    Login and SignUp APIs Completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {User, UserDocument} from "./schemas/user.schema";
 import * as bcrypt from 'bcryptjs'
 import {JwtService} from '@nestjs/jwt'
 import {Model} from "mongoose";
 import {SignUpDto} from "./dto/signup.dto";
+import {LoginDto} from "./dto/login.dto";
 
 @Injectable()
 export class AuthService {
@@ -26,6 +27,26 @@ export class AuthService {
         })
 
         const token = this.jwtService.sign({id: user._id})
+
+        return {token}
+    }
+
+    async login(loginDto: LoginDto): Promise<{ token:string }>{
+        const { email, password } = loginDto;
+
+        const user = await this.userModel.findOne({email})
+
+        if (!user){
+            throw new UnauthorizedException('Invalid Email')
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user["password"])
+
+        if (!isPasswordMatched){
+            throw new UnauthorizedException('Invalid Password')
+        }
+
+        const token = this.jwtService.sign({id:user["_id"]});
 
         return {token}
     }
